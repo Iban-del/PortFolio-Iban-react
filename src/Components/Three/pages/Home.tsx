9f8b1b576e4f7214c00a00dcf5446ccd64863948
iban <deletoille.iban@gmail.com>
2025-06-01 01:17:24 +0200

    iban

--- src/Components/Three/pages/Home.tsx
+++ src/Components/Three/pages/Home.tsx
@@ -4,7 +4,7 @@ import TextComponent from "../UI/Text"
 import ParticleGroup from "../Custom/ParticleGroup"
 import type { PagesProps } from "../Core/Interface"
 import ScrollStage from "../Scroll/ScrollStage"
-// import Smoke from "../Custom/Smoke"
+import Smoke from "../Custom/Smoke"
 
 
 
@@ -26,7 +26,7 @@ const Home  = ({
         >
             <ScrollStage coordinate={[0,position,10]} stateScroll={0}/>
             <TextComponent text="Welcome" color="#fff"/>
-            {/* <Smoke position={[0,0,-1]} opacity={1} color="#fff" /> */}
+            <Smoke position={[0,0,-1]} opacity={1} color="#fff" />
             {particleGroup}
             
         </GroupComponent>
